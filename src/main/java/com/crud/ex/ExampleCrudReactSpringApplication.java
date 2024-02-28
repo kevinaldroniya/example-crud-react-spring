c31418877fcbf5bf55750ad21c9c7808600769ec
Kevin Aldroniya <kevinaldroniya13@gmail.com>
2024-02-28 10:42:50 +0700

    setup

---
+++ src/main/java/com/crud/ex/ExampleCrudReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.crud.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleCrudReactSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleCrudReactSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/ex/ExampleCrudReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleCrudReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
